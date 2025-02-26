8c1b4bb74fd0f4255e2a2c1192881b28a6d0c50f
ognjenstanic22 <ognjen.stanic.22@singimail.rs>
2025-02-26 13:45:31 +0100

    Added angular materials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
